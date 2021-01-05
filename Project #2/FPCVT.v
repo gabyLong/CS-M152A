48184846aad67d0718f85bd1ff11e6e1361352a7
Gabriella Long <46983549+gabyLong@users.noreply.github.com>
2021-01-04 19:43:47 -0800

    Add files via upload

---
+++ Project #2/FPCVT.v
@@ -0,0 +1,228 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:			Gabriella Long
+//							605291133
+// 
+// Create Date:		01:14:24 04/22/2020 
+// Design Name: 
+// Module Name:		FPCVT 
+// Project Name: 		Project 2
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module FPCVT(
+	input wire [12:0] D,
+	output wire S,
+	output wire [2:0] E,
+	output wire [4:0] F
+	);
+	
+	wire [12:0] dBetweenMods;
+	wire [2:0] eBetweenMods;
+	wire [4:0] fBetweenMods;
+	wire sixthSig; //need for rounding
+	
+	
+	block1 b1(.dInput(D), .dOutput(dBetweenMods),.sOutput(S));
+	
+	block2 b2(.dInput(dBetweenMods), .eOutput(eBetweenMods), .fOutput(fBetweenMods), .sigSix(sixthSig));
+	
+	block3 b3(.fInput(fBetweenMods), .sixthS(sixthSig), .eInput(eBetweenMods), .fOutput(F), .eOutput(E));
+endmodule
+
+module block1(
+	input [12:0] dInput,
+	output reg [12:0] dOutput,
+	output sOutput
+	);
+
+	always @ (*)
+	begin
+		if (dInput[12] == 1)
+			dOutput = ~dInput + 1'b1;
+		else
+			dOutput = dInput;
+	end
+	
+	assign sOutput = dInput[12];
+endmodule
+
+module block2(
+	input wire [12:0] dInput, 
+	output reg [2:0] eOutput, 
+	output reg [4:0] fOutput,
+	output reg sigSix
+	);
+	
+	always @ (*)
+	begin
+		if (dInput[12] == 1)
+			eOutput = 3'b111;
+		else
+		begin
+			if (dInput[11] == 1)
+				eOutput = 3'b111;
+			else
+			begin
+				if (dInput[10] == 1)
+					eOutput = 3'b110;
+				else
+				begin
+					if (dInput[9] == 1)
+						eOutput = 3'b101;
+					else
+					begin
+						if (dInput[8] == 1)
+							eOutput = 3'b100;
+						else
+						begin
+							if (dInput[7] == 1)
+								eOutput = 3'b011;
+							else
+							begin
+								if (dInput[6] == 1)
+									eOutput = 3'b010;
+								else
+								begin
+									if (dInput[5] == 1)
+										eOutput = 3'b001;
+									else
+										eOutput = 3'b000;
+								end
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+	
+	always @ (*)
+	begin
+		if (dInput[12] == 1)
+			fOutput = 4'b1111;
+		else
+		begin
+			if (dInput[11] == 1)
+				fOutput = dInput[11:7];
+			else
+			begin
+				if (dInput[10] == 1)
+					fOutput = dInput[10:6];
+				else
+				begin
+					if (dInput[9] == 1)
+						fOutput = dInput[9:5];
+					else
+					begin
+						if (dInput[8] == 1)
+							fOutput = dInput[8:4];
+						else
+						begin
+							if (dInput[7] == 1)
+								fOutput = dInput[7:3];
+							else
+							begin
+								if (dInput[6] == 1)
+									fOutput = dInput[6:2];
+								else
+								begin
+									if (dInput[5] == 1)
+										fOutput = dInput[5:1];
+									else
+										fOutput = dInput[4:0];
+								end
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+
+	always @ (*)
+	begin
+		if (dInput[12] == 1)
+			sigSix = 1;
+		else
+		begin
+			if (dInput[11] == 1)
+				sigSix = dInput[6];
+			else
+			begin
+				if (dInput[10] == 1)
+					sigSix = dInput[5];
+				else
+				begin
+					if (dInput[9] == 1)
+						sigSix = dInput[4];
+					else
+					begin
+						if (dInput[8] == 1)
+							sigSix = dInput[3];
+						else
+						begin
+							if (dInput[7] == 1)
+								sigSix = dInput[2];
+							else
+							begin
+								if (dInput[6] == 1)
+									sigSix = dInput[1];
+								else
+								begin
+									if (dInput[5] == 1)
+										sigSix = dInput[0];
+									else
+										sigSix = 0;
+								end
+							end
+						end
+					end
+				end
+			end
+		end
+	end 
+endmodule
+
+module block3(
+	input [4:0] fInput,
+	input wire sixthS,
+	input [2:0] eInput,
+	output reg [4:0] fOutput,
+	output reg [2:0] eOutput
+	);
+	
+	always @ (*) 
+	begin
+		if (sixthS == 1)
+		begin
+			if (fInput == 5'b11111)
+			begin
+				if (eInput == 3'b111)
+				begin
+					eOutput = 3'b111;
+					fOutput = 5'b11111;
+				end
+				else
+				begin
+					eOutput = eInput + 1;
+					fOutput = 5'b10000;
+				end
+			end
+			else
+			begin
+				eOutput = eInput;
+				fOutput = fInput + 1;
+			end
+		end
+		else
+		begin //no rounding required
+			fOutput = fInput;
+			eOutput = eInput;
+		end
+	end
+endmodule
+
---
+++ Project #2/testbench_605291133.v
@@ -0,0 +1,69 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   19:20:04 04/26/2020
+// Design Name:   FPCVT
+// Module Name:   /home/ise/Project2/testbench_605291133.v
+// Project Name:  Project2
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: FPCVT
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module testbench_605291133;
+
+	// Inputs
+	reg [12:0] D;
+
+	// Outputs
+	wire S;
+	wire [2:0] E;
+	wire [4:0] F;
+
+	// Instantiate the Unit Under Test (UUT)
+	FPCVT uut (
+		.D(D), 
+		.S(S), 
+		.E(E), 
+		.F(F)
+	);
+
+	initial begin
+		// Initialize Inputs
+		D = 0;
+
+		// Wait 100 ns for global reset to finish
+		//Used values from project guide to check output
+		#100;
+      D = 1;
+		
+		#100;
+		D = 422;
+		
+		#100;
+		D = -104;
+		
+		#100;
+		D = 140;
+		
+		#100;
+		D = 0;
+		
+		#200 $finish;
+ 
+	end
+      
+endmodule
+
